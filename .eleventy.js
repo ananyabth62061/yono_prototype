a98fc5cb76702178abbbb774bcecee2c536673b1
ananyabth62061 <ananyabth62061@gmail.com>
2025-04-21 13:47:04 +0530

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/assets/images/");
+
+  eleventyConfig.addCollection("games", function (collectionApi) {
+    return collectionApi.getFilteredByTag("game");
+  });
+  
+  eleventyConfig.addCollection("featured", function (collectionApi) {
+    return collectionApi.getFilteredByTag("featured");
+  });
+  
+
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    },
+    templateFormats: ["njk", "html", "md"]
+  };
+};
---
+++ src/game_details/yono_archade.md
@@ -0,0 +1,9 @@
+---
+title: "Yono Arcade"
+tags: ["game", "featured"]
+logo: "img/yono-arcade-apk-logo.webp"
+bonus: "₹40"
+withdraw: "₹100"
+link: "yono-arcade/index.html"
+rank: 1
+---
